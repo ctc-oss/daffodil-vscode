8ea9eae864edb2b41fd39c9842d0bb28f1a9fb03
Shane Dell <32347414+Shanedell@users.noreply.github.com>
2022-02-23 15:59:22 -0500

    Got an example omega-edit client working

--- build/extension.webpack.config.js
+++ build/extension.webpack.config.js
@@ -17,7 +17,7 @@ module.exports = /** @type WebpackConfig */ {
 	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 	target: 'node', // vscode extensions run in a Node.js-context
 	entry: {
-		extension: './src/adapter/extension.ts'
+		extension: './out/adapter/extension.js'
 	},
 	resolve: { // support reading TypeScript and JavaScript files
 		extensions: ['.ts', '.js']
