7b2779255d0927eb8f87694516b8f1cf3358155f
Robert Strickland <strickland.rbls@gmail.com>
2024-05-17 11:45:52 -0500

    Added mediator BDD tests

--- src/dataEditor/include/mediator/editorMediator.ts
+++ src/dataEditor/include/mediator/editorMediator.ts
@@ -14,3 +14,20 @@ export abstract class IEditorComponent {
     readonly componentId: string
   ) {}
 }
+
+export interface MediatorEvent<T> {
+  readonly type: T
+  content(): any
+  toString(): string
+}
+export type MediatorEventListener<D> = (content: D) => void
+export interface IMediator<T> {
+  notify(event: MediatorEvent<T>): void
+  register<D>(type: T, handler: MediatorEventListener<D>[]): void
+}
+export abstract class IMediatorComponent<T> {
+  constructor(protected mediator: IMediator<T>) {
+    this.registerEventHandlers()
+  }
+  protected abstract registerEventHandlers(): void
+}
